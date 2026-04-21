64a5e5d8ddcd6c17c32741c79221fdd7f7ea5740
Edwin Game Dev <edwinjonesholanda@gmail.com>
2026-04-21 12:16:55 +0100

    hook player inputs and joystick

--- Assets/Scripts/InputSystem/PlayerInput.cs
+++ Assets/Scripts/InputSystem/PlayerInput.cs
@@ -1,19 +1,36 @@
 using EdwinGameDev.Characters;
+using EdwinGameDev.Utils.Joystick;
 using UnityEngine;
 
 namespace EdwinGameDev.InputSystem
 {
     public class PlayerInput : MonoBehaviour
     {
+        private Joystick _joystick;
         private CharacterControllerFacade _character;
+
+        private void Awake()
+        {
+            _joystick = FindObjectOfType<Joystick>();
+        }
 
         private void Update()
         {
-            Vector3 input = new Vector3(
-                Input.GetAxisRaw("Horizontal"),
-                0,
-                Input.GetAxisRaw("Vertical")
-            );
+            Vector3 input;
+
+            // Use joystick if available and being used, otherwise fall back to keyboard
+            if (_joystick && _joystick.Direction.sqrMagnitude > 0.01f)
+            {
+                input = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+            }
+            else
+            {
+                input = new Vector3(
+                    Input.GetAxisRaw("Horizontal"),
+                    0,
+                    Input.GetAxisRaw("Vertical")
+                );
+            }
 
             _character?.SetDirection(input);
         }
